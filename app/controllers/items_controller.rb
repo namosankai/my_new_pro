4198ce97ca95eeb090ac1736c1f2f79d0114bc69
namosankai <lk2g37@gmail.com>
2020-09-21 15:27:03 +0900

    20200921

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,13 +8,21 @@ class ItemsController < ApplicationController
   #traders/newの中にあるitem一覧
   def new
     @trader = Trader.find_by(id: params[:id])
+    @item = Item.find_by(id: params[:id])
     @trader_id = Item.find_by(trader_id: params[:trader_id])
+    
     
   end
 
   #各traderごとの新規item追加画面
   def create
-    @item = Item.new(name: params[:name], price: params[:price], cost: params[:cost], trader_id: params[:trader_id])
+    @item = Item.new(
+      name: params[:name],
+      price: params[:price],
+      cost: params[:cost],
+      trader_id: params[:trader_id]
+    )
+    
     
     if @item.save
       flash[:notice] = "新規アイテムを追加しました"
